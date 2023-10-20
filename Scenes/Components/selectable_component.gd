7dc10d6c7be18caa576ce5d1c23e85945dc3e099
Joshua Crews <joshuacrews16@outlook.com>
2023-10-21 00:53:48 +1000

    Update selectable_component.gd
    - Editing collision layers in inspector

--- Scenes/Components/selectable_component.gd
+++ Scenes/Components/selectable_component.gd
@@ -9,11 +9,6 @@ extends Area2D
 var is_selected : bool :
 	get:
 		return selector.selected_object == selectable
-
-func _ready():
-	self.set_collision_layer_value(1, false)
-	self.set_collision_layer_value(2, true)
-	self.set_collision_mask_value(1, true)
 	
 func _input_event(viewport, event: InputEvent, shape_idx):
 	if event.is_action_pressed("right_click"):
