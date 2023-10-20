b433fbdd754e3b93d595da91c0a9f160963bb348
Joshua Crews <joshuacrews16@outlook.com>
2023-10-21 00:53:23 +1000

    Update hitbox_component.gd
    - Removed health component dependency
    - Editing collision layers in inspector

--- Scenes/Components/hitbox_component.gd
+++ Scenes/Components/hitbox_component.gd
@@ -2,16 +2,6 @@ class_name HitboxComponent
 extends Area2D
 
 signal on_hitbox_entered(area)
-
-@export var health_component : HealthComponent = null
-
-func _ready():
-	self.set_collision_layer_value(1, true)
-	self.set_collision_mask_value(1, true)
 	
-func damage(attack: Attack):
-	if health_component:
-		health_component.damage(attack)
-
 func _on_area_entered(area):
 	on_hitbox_entered.emit(area)
