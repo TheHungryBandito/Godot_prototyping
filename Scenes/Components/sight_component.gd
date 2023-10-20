bc2d68b534f96aa5680f41cdebc0e7ba66652539
Joshua Crews <joshuacrews16@outlook.com>
2023-10-21 00:55:43 +1000

    Update sight_component.gd
    - Editing collision layers in inspector
    - Avoid adding self to sight

--- Scenes/Components/sight_component.gd
+++ Scenes/Components/sight_component.gd
@@ -4,14 +4,14 @@ extends Area2D
 signal on_sight_entered(area)
 signal on_sight_exited(area)
 
-var objects_in_sight: Array = []
+var objects_in_sight: Array[Node2D] = []
 
 func _ready():
-	self.set_collision_layer_value(1, false)
-	self.set_collision_layer_value(2, true)
-	self.set_collision_mask_value(1, true)
+	objects_in_sight.clear()
 	
 func _on_area_entered(area: Area2D):
+	if area.owner == owner:
+		return
 	if objects_in_sight.has(area.owner):
 		return
 	objects_in_sight.append(area.owner)
@@ -22,3 +22,32 @@ func _on_area_exited(area: Area2D):
 		return
 	objects_in_sight.remove_at(objects_in_sight.find(area.owner))
 	on_sight_exited.emit(area.owner)
+
+func get_closest() -> Node2D:
+	if objects_in_sight.is_empty():
+		return null
+	var closest = null
+	var distance = 9999.00
+	var duplicate_array = objects_in_sight.duplicate()
+	for visible_object in objects_in_sight:
+		if position.distance_to(visible_object.position) < distance:
+			distance = position.distance_to(visible_object.position)
+			closest = visible_object
+	return closest
+
+func get_furthest() -> Node2D:
+	if objects_in_sight.is_empty():
+		return null
+	var furthest = null
+	var distance = 0
+	var duplicate_array = objects_in_sight.duplicate()
+	for visible_object in objects_in_sight:
+		if position.distance_to(visible_object.position) > distance:
+			distance = position.distance_to(visible_object.position)
+			furthest = visible_object
+	return furthest
+
+func get_random() -> Node2D:
+	if objects_in_sight.is_empty():
+		return null
+	return objects_in_sight.pick_random()
