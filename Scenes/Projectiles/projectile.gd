be4a7b7b9db1a7be61aa22a6a4cc59ec52e65099
Joshua Crews <joshuacrews16@outlook.com>
2023-10-21 21:15:06 +1000

    Update projectile.gd
    - now extends Entity
    - abstracted death function from health component
    - projectiles can now be damaged
    - abstracted checking if collision is a target into its own function.

--- Scenes/Projectiles/projectile.gd
+++ Scenes/Projectiles/projectile.gd
@@ -1,5 +1,5 @@
 class_name Projectile
-extends Node2D
+extends Entity
 
 @onready var movement_component: MovementComponent = get_node("MovementComponent")
 @onready var health_component: HealthComponent = get_node("HealthComponent")
@@ -12,19 +12,25 @@ var is_enabled = false
 
 func _ready():
 	hitbox_component.on_hitbox_entered.connect(collide_projectile)
+	health_component.on_die.connect(_on_die)
 	
 func _process(delta):
 	if !is_enabled:
 		return
 	movement_component.move(delta, speed, destination)
+	self.look_at(destination)
 	
 	if position.distance_to(destination) < 5:
 		health_component.die()
+		
+func _on_die():
+	queue_free()
 	
+func damage(attack: Attack):
+	health_component.damage(attack)
 	
 func send_projectile(attack: Attack, to: Vector2):
 	self.attack = attack
-	self.look_at(to)
 	position = attack.attack_position
 	destination = to
 	is_enabled = true
@@ -35,12 +41,20 @@ func collide_projectile(area):
 		return
 	if attack == null:
 		return
-	if area.owner == attack.sender:
-		return
-	if !area.owner.has_method("damage"):
+	if not is_collision_target(area):
 		return
 	area.owner.damage(attack)
 	health_component.die()
+
+func is_collision_target(area) -> bool:
+	if area.owner == attack.sender:
+		return false
+	if !area.owner.has_method("damage"):
+		return false
+	return true
+
+func get_object_type():
+	return "Projectile"
 
 
 
